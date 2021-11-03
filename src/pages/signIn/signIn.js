bab070f23a01b9d6844c1091e4d63f36e8ce470a
binh mai <maivanbinh1321999@gmail.com>
2021-11-03 08:26:59 +0700

    init

--- src/pages/signIn/signIn.js
+++ src/pages/signIn/signIn.js
@@ -2,7 +2,36 @@ import React from "react";
 import "./signIn.css";
 
 const signIn = () => {
-  return <> Sign in</>;
+  return (
+    <div className="login">
+      <form>
+
+        <div className="login-container">
+          <label for="uname">
+            <b>Username</b>
+          </label>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            name="uname"
+            required
+          />
+
+          <label for="psw">
+            <b>Password</b>
+          </label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            name="psw"
+            required
+          />
+
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default signIn;
