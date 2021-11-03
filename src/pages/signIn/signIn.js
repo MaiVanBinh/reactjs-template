0e87366d8c6454a5f656a85c205c2b948e212487
binh mai <maivanbinh1321999@gmail.com>
2021-11-03 09:09:48 +0700

    update llogin function

--- src/pages/signIn/signIn.js
+++ src/pages/signIn/signIn.js
@@ -1,11 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "./signIn.css";
 
-const signIn = () => {
+const SignIn = () => {
+  // add state username
+  const [username, setUsername] = useState({
+    value: "",
+    touch: false,
+  });
+  // add state password
+  const [password, setPassword] = useState({
+    value: "",
+    touch: false,
+  });
+  const [errMessage, setErrMessage] = useState('');
+
+  // add login handler function
+  const loginHandler = (e) => {
+    if (username.value === "") {
+      setUsername({
+        ...username,
+        touch: true,
+      });
+      return;
+    }
+    if (password.value === "") {
+      setUsername({
+        ...username,
+        touch: true,
+      });
+      return;
+    }
+    const payload = {
+      username: username.value,
+      password: password.value,
+    };
+    console.log(payload)
+    fetch('http://localhost:3000/users/login', {
+      method: "POST",
+      headers: {
+        'content-type':  'application/json'
+      },
+      body: JSON.stringify(payload)
+    }).then(res => res.json()).then(resData => {
+      console.log(resData)
+    }).catch(err => setErrMessage("Username or Password wrong"));
+  };
   return (
     <div className="login">
-      <form>
-
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          loginHandler();
+        }}
+      >
+        {errMessage}
         <div className="login-container">
           <label for="uname">
             <b>Username</b>
@@ -14,24 +62,40 @@ const signIn = () => {
             type="text"
             placeholder="Enter Username"
             name="uname"
+            value={username.value}
+            onChange={(e) =>
+              setUsername({
+                value: e.target.value,
+                touch: true,
+              })
+            }
             required
           />
-
+          <p>username is not empty</p>
           <label for="psw">
             <b>Password</b>
           </label>
           <input
             type="password"
             placeholder="Enter Password"
+            value={username.password}
+            onChange={(e) =>
+              setPassword({
+                value: e.target.value,
+                touch: true,
+              })
+            }
             name="psw"
             required
           />
-
-          <button type="submit">Login</button>
+          <p>password is not empty</p>
+          <button type="submit" onClick={loginHandler}>
+            Login
+          </button>
         </div>
       </form>
     </div>
   );
 };
 
-export default signIn;
+export default SignIn;
---
+++ src/reduxConfig/reducer/users.js
@@ -0,0 +1,11 @@
+const initState = null; 
+
+const reducer = (state = initState, action) => {
+  
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
